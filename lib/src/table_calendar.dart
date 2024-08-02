b66976f59d229c744835ccea94fc906591b8d2b3
shibinnvc <mhdshibinmkd@gmail.com>
2024-08-02 11:32:13 +0530

    add custom function

--- lib/src/table_calendar.dart
+++ lib/src/table_calendar.dart
@@ -204,6 +204,9 @@ class TableCalendar<T> extends StatefulWidget {
   /// Called when the calendar is created. Exposes its PageController.
   final void Function(PageController pageController)? onCalendarCreated;
 
+  //Customize function _onLeftChevronTap
+  final void Function()? onLeftChevronTap;
+
   /// Creates a `TableCalendar` widget.
   TableCalendar({
     Key? key,
@@ -260,6 +263,7 @@ class TableCalendar<T> extends StatefulWidget {
     this.onPageChanged,
     this.onFormatChanged,
     this.onCalendarCreated,
+    this.onLeftChevronTap,
   })  : assert(availableCalendarFormats.keys.contains(calendarFormat)),
         assert(availableCalendarFormats.length <= CalendarFormat.values.length),
         assert(weekendDays.isNotEmpty
@@ -432,6 +436,7 @@ class _TableCalendarState<T> extends State<TableCalendar<T>> {
   }
 
   void _onLeftChevronTap() {
+    widget.onLeftChevronTap!();
     _pageController.previousPage(
       duration: widget.pageAnimationDuration,
       curve: widget.pageAnimationCurve,
