78cd3830836210c2cfaf14d01984e27e522b883b
karthik <karthiksyamji@prismcloudinc.com>
2024-09-04 09:39:34 -0400

    time zone patches

--- lib/src/table_calendar.dart
+++ lib/src/table_calendar.dart
@@ -381,7 +381,7 @@ class _TableCalendarState<T> extends State<TableCalendar<T>> {
         }
       }
     } else {
-      widget.onDaySelected?.call(day.toLocal(), _focusedDay.value.toLocal());
+      widget.onDaySelected?.call(day, _focusedDay.value);
     }
   }
 
@@ -410,7 +410,7 @@ class _TableCalendarState<T> extends State<TableCalendar<T>> {
           widget.onRangeSelected!(_firstSelectedDay, null, _focusedDay.value);
         } else {
           _firstSelectedDay = null;
-          widget.onDaySelected?.call(day.toLocal(), _focusedDay.value.toLocal());
+          widget.onDaySelected?.call(day, _focusedDay.value);
         }
       }
     }
